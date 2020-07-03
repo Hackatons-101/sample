38f85f66ba9110c3698c0dba32a1340729d1f50d
Ashmit Sachan <66499974+ashmit-sachan@users.noreply.github.com>
2020-07-03 18:10:10 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+<?php include('side-nav_doc.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Date Picker</title>
+    <style>
+    .image-info-wrap{
+        width: 170px;
+        margin-left: calc(50% - 85px);
+    }
+    </style>
+
+</head>
+<body>
+
+    <div class="image-info-wrap">
+    <img scr="avatar2.png" style="height: 150px; width: 150 px;">
+    <h2>NAME OF DOCTOR</h2>
+    </div>
+
+</body>
